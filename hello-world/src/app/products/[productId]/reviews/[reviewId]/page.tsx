58040d159d7a2effeb5a1b2e14af96935e3bb1d4
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-04-06 20:01:44 +0300

    :sparkles: add custom error flow

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,15 +1,30 @@
-import { notFound } from "next/navigation"
+import { notFound } from "next/navigation";
 
-export default function Review({ params }: {
-    params: {
-        productId: string,
-        reviewId: string
-    }
-})
-{
-    if (parseInt(params.reviewId) > 1000) {
-        notFound()
-    }
+function getRandomInt(count: number) {
+	return Math.floor(Math.random() * count);
+}
 
-    return <h1>Review {params.reviewId} for Product {params.productId}</h1>
+export default function Review({
+	params,
+}: {
+	params: {
+		productId: string;
+		reviewId: string;
+	};
+}) {
+	const random = getRandomInt(2);
+
+	if (random === 1) {
+		throw new Error("Error loading review");
+	}
+
+	if (parseInt(params.reviewId) > 1000) {
+		notFound();
+	}
+
+	return (
+		<h1>
+			Review {params.reviewId} for Product {params.productId}
+		</h1>
+	);
 }
