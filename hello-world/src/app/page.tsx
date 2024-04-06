39b3cc4b8712a6d3373e1c7b3a576b70ebac4086
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-04-06 21:11:14 +0300

    :sparkles: add parallel routes

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -4,10 +4,21 @@ export default function Home() {
 	return (
 		<>
 			<h1>Welcome home!</h1>
-			<Link href="/blog">Blog</Link>
-			<Link href="/products">Products</Link>
-			<Link href="/login">Login</Link>
-			<Link href="/order-product">Order Product</Link>
+			<h2>
+				<Link href="/blog">Blog</Link>
+			</h2>
+			<h2>
+				<Link href="/products">Products</Link>
+			</h2>
+			<h2>
+				<Link href="/login">Login</Link>
+			</h2>
+			<h2>
+				<Link href="/order-product">Order Product</Link>
+			</h2>
+			<h2>
+				<Link href="/complex-dashboard">Complex Dashboard</Link>
+			</h2>
 		</>
 	);
 }
