1d00aed543792db1fdbde0f04c9687baadb1cbf3
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-04-06 19:29:29 +0300

    :lipstick: add links for navigation

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -1,4 +1,11 @@
-export default function Home()
-{
-    return <h1>Welcome home!</h1>
+import Link from "next/link";
+
+export default function Home() {
+	return (
+		<>
+			<h1>Welcome home!</h1>
+			<Link href="/blog">Blog</Link>
+			<Link href="/products">Products</Link>
+		</>
+	);
 }
--- hello-world/src/app/products/page.tsx
+++ hello-world/src/app/products/page.tsx
@@ -1,9 +1,22 @@
-export default function Products()
-{
-    return <ol>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-        <li>Product 4</li>
-    </ol>
+import Link from "next/link";
+
+export default function Products() {
+	const productId = 100;
+
+	return (
+		<>
+			<Link href="/">Home</Link>
+			<h2>
+				<Link href="/products/1">Product 1</Link>
+			</h2>
+			<h2>
+				<Link href="/products/2">Product 2</Link>
+			</h2>
+			<h2>
+				<Link href={`/products/${productId}`} replace>
+					Product {productId}
+				</Link>
+			</h2>
+		</>
+	);
 }
