17816c34fc4644c30ed10b3280028c79096145ee
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-04-08 23:55:13 +0300

    :sparkles: handle PATCH requests

--- route-handlers/src/app/comments/[id]/route.ts
+++ route-handlers/src/app/comments/[id]/route.ts
@@ -5,3 +5,13 @@ export async function GET(_request: Request, { params }: { params: { id: string 
 
 	return Response.json(comment);
 }
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+	const body = await request.json();
+	const { text } = body;
+
+	const index = comments.findIndex((comment) => comment.id === parseInt(params.id));
+	comments[index].text = text;
+
+	return Response.json(comments[index]);
+}
