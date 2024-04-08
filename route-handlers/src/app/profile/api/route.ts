c5aeeab670f938bed0d654e2e6e19002adf6942a
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-04-09 00:25:44 +0300

    :sparkles: add support for headers

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,13 @@
-export async function GET() {
-	return new Response("Profile");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+	const headerList = headers();
+	console.log(headerList.get("Authorization"));
+
+	return new Response("<h1>Profile</h1>", {
+		headers: {
+			"Content-Type": "text/html",
+		},
+	});
 }
