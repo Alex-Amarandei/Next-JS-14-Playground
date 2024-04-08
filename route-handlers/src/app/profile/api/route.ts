c655dbb6b8439914de987276adac9e209bb90c11
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-04-09 00:30:38 +0300

    :sparkles: add support for cookies

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,13 +1,19 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
 	const headerList = headers();
 	console.log(headerList.get("Authorization"));
 
+	cookies().set("resultsPerPage", "20");
+
+	const theme = cookies().get("theme");
+	console.log(theme);
+
 	return new Response("<h1>Profile</h1>", {
 		headers: {
 			"Content-Type": "text/html",
+			"Set-Cookie": "theme=dark",
 		},
 	});
 }
